f11c2fe0c7dfc19525833fa8e31df3525b0bdca5
FacundoIsoler <facundoisoler@outlook.com>
2024-02-10 09:30:34 -0300

    Primer commit

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+function About() {
+    return (
+        <div>Esta es la página del about</div>
+    )
+}
+
+export default About
---
+++ pages/products/create.js
@@ -0,0 +1,71 @@
+import { useState } from "react"
+
+function Create() {
+    const initialState = { name: "", price: 0 }
+    const [product, setProduct] = useState(initialState)
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        const inputValue = e.target.value;
+        const inputName = e.target.name;
+
+
+        setProduct({ ...product, [inputName]: inputValue })
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        fetch('stock-app-commerce.azurewebsites.net/api/v1/products', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(product)
+        })
+            .then(res => res.json)
+            .then((data) => {
+                setProduct(initialState),
+                console.log("Producto creado con exito")
+            })
+            .catch(err => {
+                console.log({err})
+            })
+    }
+
+
+
+    return (
+        <>
+            <div>
+                <h1> Crear nuevo producto</h1>
+                <form>
+                    <input onChange={handleChange} type="text" placeholder="Nombre del producto..." value={product.name} name="name" />
+                    <input onChange={handleChange} type="number" placeholder="Precio del producto..." value={product.price} name="price" />
+                    <button onClick={handleClick}>Crear Producto</button>
+                </form>
+            </div>
+            <style jsx>
+                {
+                    `
+                    form{
+                        display:flex;
+                        flex-direction:column;
+                        width:20rem;
+                        margin:0 auto;
+                    }
+
+                    input{
+                        margin-bottom:0.5rem;
+                    }
+
+                    h1{
+                        text-align: center;
+                    }
+                    `
+                }
+            </style>
+        </>
+    )
+}
+
+export default Create
